a8a4a9d4fb1ed2d050377b1a4bfd21baa3e3c05b
February110 <anhtai1879@gmail.com>
2026-01-19 23:41:32 +0700

    fix database

---
+++ class_solution/class.Infrastructure/Migrations/20260119163923_AddAssignmentGroupMode.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace class_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAssignmentGroupMode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GroupMode",
+                table: "Assignments",
+                type: "nvarchar(16)",
+                maxLength: 16,
+                nullable: true,
+                defaultValue: "student");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GroupMode",
+                table: "Assignments");
+        }
+    }
+}
